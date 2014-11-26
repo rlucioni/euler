ca0a7143d9eea576c8bf304158c9bd0930e080a1
Renzo Lucioni <renzo@renzolucioni.com>
2014-11-25 20:21:34 -0500

    Solve problem 3

--- euler/solutions/solution_3.py
+++ euler/solutions/solution_3.py
@@ -32,7 +32,11 @@ def sieve_of_eratosthenes(limit):
 
 
 def largest_prime_factor(number):
-    """Find the largest prime factor of the given number."""
+    """Find the largest prime factor of the given number.
+
+    This approach works, but the sieve generator starts running into
+    space/memory constraints when given large numbers (like 600851475143).
+    """
     primes = sieve_of_eratosthenes(number)
 
     for prime in primes:
@@ -41,12 +45,34 @@ def largest_prime_factor(number):
             # Divide number by current prime
             number /= prime
 
-        # Check if we've just divided by the biggest prime
+        # Check if we've just divided by the largest prime.
         if number is 1:
             return prime
 
 
+def largest_prime_factor_improved(number):
+    """Find the largest prime factor of the given number."""
+    for candidate in xrange(2, number):
+        # Extract as many of the current factor as we can from
+        # the number. This approach essentially applies Eratosthenes'
+        # method inline! Since we start with 2, the first and smallest
+        # prime, only primes will be able to successfully factor the
+        # given number. This means we don't need to generate a huge
+        # array in which to store primality flags.
+        while number % candidate is 0:
+            # Divide number by current prime
+            number /= candidate
+
+        # Check if we've just divided by the largest prime.
+        if number is 1:
+            return candidate
+
+
 number = int(sys.argv[1])
 
+# Seconds to execute 1000 times when finding the largest factor of 13195: 1.23636722565
 # timed_execution.timed_execution(largest_prime_factor, number)
-print largest_prime_factor(number)
+
+# Seconds to execute 1000 times when finding the largest factor of 13195: 0.00467586517334
+# Seconds to execute 1000 times when finding the largest factor of 600851475143: 0.826802968979 <- crazy!
+timed_execution.timed_execution(largest_prime_factor_improved, number)
