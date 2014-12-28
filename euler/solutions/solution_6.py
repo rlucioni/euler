f28e5afaaf32953d4bf6178841ce2b55d8adb0b4
Renzo Lucioni <renzo@renzolucioni.com>
2014-12-27 20:40:37 -0800

    Solve problem 6

---
+++ euler/solutions/solution_6.py
@@ -0,0 +1,38 @@
+"""Sum square difference
+
+The sum of the squares of the first ten natural numbers is,
+
+1**2 + 2**2 + ... + 10**2 = 385
+
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)**2 = 55**2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+"""
+import sys
+
+from ..utilities import timed_execution
+
+
+def sum_square_difference(ceiling):
+    """Compute the difference between the sum of squares and the square of
+    the sum of the natural numbers up to and including the provided ceiling.
+    """
+    numbers = range(ceiling + 1)
+
+    sum_squares = sum(map(lambda number: number**2, numbers))
+    square_sum = sum(numbers)**2
+
+    sum_square_difference = square_sum - sum_squares
+
+    return sum_square_difference
+
+
+ceiling = int(sys.argv[1])
+
+timed_execution.timed_execution(sum_square_difference, ceiling)
